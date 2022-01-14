42d03753846c4133c4049a1059460755beef2283
yozhgoor <yozhgoor@outlook.com>
2022-01-14 17:55:57 +0100

    Update to start 2022

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,8 @@
 //! You can also send me an
 //! [email](mailto:yohan.boogaert+recruit@protonmail.com).
 
+// Jobs
+
 /// Junior at RustMinded - Current
 pub mod software_developer {}
 
@@ -22,42 +24,91 @@ pub mod emergency_medical_technician {}
 /// Ambulance, Shuttle, Taxi,... - 2014 to 2019
 pub mod professional_driver {}
 
+// Personal Projects
+
 /// A CLI tool that allows you to create a temporary new Rust project using
 /// cargo with already installed dependencies.
 /// [\[Repository\]](<https://github.com/rustminded/cargo-temp>)
-pub struct CargoTemp {}
+#[macro_export]
+macro_rules! cargo_temp {
+    () => {};
+}
 
 /// My vision of the Rust Programming Language as an introduction to Rust.
 /// [\[Repository\]](<https://github.com/yozhgoor/intro-to-rust>)
-pub struct IntroToRust {}
+#[macro_export]
+macro_rules! intro_to_rust {
+    () => {};
+}
 
-/// A Yew and Yewprint playground using wasmbl.
+/// A Yew and Yewprint playground using wasm-run.
 /// [\[Repository\]](<https://github.com/yozhgoor/yewprint-playground>)
-pub struct YewprintPlayground {}
+#[macro_export]
+macro_rules! yewprint_playground {
+    () => {};
+}
 
-/// Port of blueprintjs.com to Yew.
+/// A crate to create and handle processes on Windows (similar too `std::process`).
+/// [\[Repository\]](<https://github.com/yozhgoor/CreateProcessW>)
+#[macro_export]
+macro_rules! CreateProcessW {
+    () => {};
+}
+
+/// Yet Another To-Do App in your Terminal. (Under development)
+/// [\[Repository\]](<https://github.com/yozhgoor/tui-do>)
+#[macro_export]
+macro_rules! tui_do {
+    () => {};
+}
+
+// OSS Contributions
+
+/// Port of [blueprintjs](<https://blueprintjs.com>) to Yew.
 /// [\[Repository\]](<https://github.com/yewprint/yewprint>)
-pub enum Yewprint {}
+pub struct Yewprint {}
 
 /// Bundles and tooling for Rust WASM frontend application.
 /// [\[Repository\]](<https://github.com/wasmbl/wasmbl>)
-pub enum Wasmbl {}
+pub struct WasmRun {}
 
 /// A CLI tool that compares a crate's public API between two different
 /// branches, shows what changed, and suggests the next version according to
 /// semver.
 /// [\[Repository\]](<https://github.com/iomentum/cargo-breaking>)
-pub enum CargoBreaking {}
+pub struct CargoBreaking {}
 
 /// French translation of the book "The Rust Programming Language".
 /// [\[Repository\]](<https://github.com/Jimskapt/rust-book-fr>)
-pub enum RustBookFr {}
+pub struct RustBookFr {}
 
 /// Third-I, the only camera that replicates your hearing and your point of
 /// view.
 /// [\[Repository\]](<https://github.com/BigBoySystems/third-i-frontend>) |
 /// [\[Site\]](<https://bigboysystems.com/>)
-pub enum ThirdiFrontend {}
+pub struct ThirdIFrontend {}
+
+/// The place to be hired as an awesome Rustacean. (Under development)
+/// [\[Repository\]](<https://github.com/rustminded/recruit-rust>)
+pub struct RecruitRust {}
+
+/// Customizable commands based on [xtask](https://github.com/matklad/cargo-xtask)
+/// for your WASM project.
+/// [\[Repository\]](<https://github.com/rustminded/xtask-wasm>)
+pub struct XtaskWasm {}
+
+// Non-technical skills
+
+/// Web application deployment using [Netlify](<https://www.netlify.com>).
+pub enum WebDeployment {}
+
+/// Continuous Integration via [GitHub Actions](<https://github.com/actions>).
+pub enum SettingUpCI {}
+
+/// Release, update and maintenance of an OSS project.
+pub enum OssProjectMaintenance {}
+
+// Technical skills
 
 /// French (5. Native), English (3. Minimum Professional Proficiency).
 pub const SPOKEN_LANGUAGES: () = ();
@@ -68,11 +119,13 @@ pub const PROGRAMMING_LANGUAGES: () = ();
 /// Yew.
 pub const FRAMEWORKS: () = ();
 
-/// Git, WASM, CLI.
+/// Git, WASM, CLI, Terminal UI.
 pub const TECHNOLOGIES: () = ();
 
 /// Arch Linux, Pop!_OS.
 pub const OS: () = ();
+
+// Personal traits
 
 pub trait TeamPlayer {}
 
