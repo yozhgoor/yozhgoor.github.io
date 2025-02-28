f70bf99b6f7af681e5f8581a76f034fc2e428781
yozhgoor <yozhgoor@users.noreply.github.com>
2025-02-28 21:40:10 +0000

    deploy: 030d61f95f13452f175a355f00e4c18e65c0d3a1

--- crates.js
+++
@@ -1,2 +0,0 @@
-window.ALL_CRATES = ["yohan_boogaert_1995"];
-//{"start":21,"fragment_lengths":[21]}
--- src-files.js
+++
@@ -1,3 +0,0 @@
-var srcIndex = new Map(JSON.parse('[["yohan_boogaert_1995",["",[],["lib.rs"]]]]'));
-createSrcSidebar();
-//{"start":36,"fragment_lengths":[42]}
